b981a8d13b52e7c0643ce502039e3f7a66abc06e
Kevin Docanto <kevin-teus@hotmail.com>
2023-08-21 22:22:22 -0300

    Typo fixed

--- app/components/gallery/index.tsx
+++ app/components/gallery/index.tsx
@@ -10,7 +10,7 @@ export default function Gallery() {
       className="bg-black text-white flex flex-col lg:justify-center items-center mt-2 pb-24"
       id="galeria"
     >
-      <h1 className="text-4xl font-bold mt-2 mb-2">Gallery</h1>
+      <h1 className="text-4xl font-bold mt-2 mb-2">Galeria</h1>
 
       <div className="flex flex-wrap justify-center items-center max-w-4xl mt-6 sm:w-full">
         <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2">
